44b7349b18772c52c4712aaad8f3d6f9efaf0eb3
iMoloy <moloykrishnapaul@gmail.com>
2026-04-01 09:10:00 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar bg-base-100 w-11/12 mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow font-semibold"
+            >
+              <li>
+                <a>Products</a>
+              </li>
+              <li>
+                <a>Features</a>
+              </li>
+              <li>
+                <a>Pricing</a>
+              </li>
+              <li>
+                <a>Testimonies</a>
+              </li>
+              <li>
+                <a>FAQ</a>
+              </li>
+            </ul>
+          </div>
+          <a className="text-3xl text-primary font-extrabold">DigiTools</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 font-semibold">
+            <li>
+              <a>Products</a>
+            </li>
+            <li>
+              <a>Features</a>
+            </li>
+            <li>
+              <a>Pricing</a>
+            </li>
+            <li>
+              <a>Testimonies</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end gap-3">
+          <div className="font-semibold">
+            <div className="flex-none">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle"
+              >
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    {" "}
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                    />{" "}
+                  </svg>
+                  <span className="badge badge-xs indicator-item">0</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a className="font-semibold">Login</a>
+          <a className="btn btn-primary rounded-4xl">Get Started</a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
